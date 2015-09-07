a082876f7b9e11daf84516e0c680295768c04b6a
Max <1@porzelt.net>
2015-09-07 22:37:55 +0200

    Add Account Item

---
+++ dyndns updater/Entities/AccountItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DynDNS_Updater.Entities
+{
+    class AccountItem
+    {
+        public string Username;
+        public string Token;
+        public string Hostname;
+
+        public bool Paused;
+        public DateTime PausedDate;
+
+        public string IPType;
+
+        public string LastUpdatedIP;
+        public DateTime LastUpdate;
+
+        public DDNSProvider Provider;
+    }
+}
